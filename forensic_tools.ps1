3d3df2a592c79fbbae4913d3e4d20aaad15fba92
AngelSayani <109420378+AngelSayani@users.noreply.github.com>
2025-03-03 20:35:45 +0530

    Update forensic_tools.ps1

--- forensic_tools.ps1
+++ forensic_tools.ps1
@@ -3,7 +3,7 @@
 # This script provides tools for registry analysis
 
 Write-Host "========================================================" -ForegroundColor Cyan
-Write-Host "Registry Forensics Lab: Tracking the Dark Kittens" -ForegroundColor Cyan
+Write-Host "Registry Forensics: Tracking the Dark Kittens Lab by Angel Sayani" -ForegroundColor Cyan
 Write-Host "Registry Forensic Tools" -ForegroundColor Cyan
 Write-Host "========================================================" -ForegroundColor Cyan
 Write-Host ""
